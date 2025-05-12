358f18441665b83551968e723c2098ab77f45e49
CayoRw <Cayorwcs@aluno.ufsj.edu.br>
2025-05-12 13:28:37 -0300

    changing base and adding details

--- compilador.py
+++ compilador.py
@@ -1,6 +1,8 @@
+#ic branched v0.0.6
+
 from pydaq.pydaq_global import PydaqGui
 
 PydaqGui()
 
 #pyside6-rcc resources_1.qrc -o resources_1_rc.py
-#ctrl shift P para selecionar o ambiente
+#ctrl shift P to select the python interpreter
